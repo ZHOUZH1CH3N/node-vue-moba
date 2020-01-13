8b1f578764326eb1d6f30738c99921f0391752c4
1393925530 <zzc1393925530@gmail.com>
2020-01-13 21:47:15 +0800

    通用CRUD接口

--- admin/src/views/CategoryList.vue
+++ admin/src/views/CategoryList.vue
@@ -30,7 +30,7 @@ export default {
   },
   methods: {
     async fetch () {
-      const res = await this.$http.get('categories')
+      const res = await this.$http.get('rest/categories')
       this.items = res.data
     },
     async remove (row) {
@@ -40,7 +40,7 @@ export default {
           type: 'warning'
         }).then(async () => {
           // eslint-disable-next-line no-unused-vars
-          const res = await this.$http.delete(`categories/${row._id}`)
+          const res = await this.$http.delete(`rest/categories/${row._id}`)
           this.$message({
             type: 'success',
             message: '删除成功!'
