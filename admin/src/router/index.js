5e94cab060c0d1a8b058b48f5112a26ecf228374
1393925530 <zzc1393925530@gmail.com>
2020-01-15 20:12:17 +0800

    文章管理/富文本编辑器引入

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -10,6 +10,9 @@ import ItemList from '../views/ItemList.vue'
 import HeroEdit from '../views/HeroEdit.vue'
 import HeroList from '../views/HeroList.vue'
 
+import ArticleEdit from '../views/ArticleEdit.vue'
+import ArticleList from '../views/ArticleList.vue'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -21,12 +24,18 @@ const routes = [
       { path: '/categories/create', component: CategoryEdit },
       { path: '/categories/edit/:id', component: CategoryEdit, props: true },
       { path: '/categories/list', component: CategoryList },
+
       { path: '/items/create', component: ItemEdit },
       { path: '/items/edit/:id', component: ItemEdit, props: true },
       { path: '/items/list', component: ItemList },
+
       { path: '/heroes/create', component: HeroEdit },
       { path: '/heroes/edit/:id', component: HeroEdit, props: true },
-      { path: '/heroes/list', component: HeroList }
+      { path: '/heroes/list', component: HeroList },
+      
+      { path: '/articles/create', component: ArticleEdit },
+      { path: '/articles/edit/:id', component: ArticleEdit, props: true },
+      { path: '/articles/list', component: ArticleList },
     ]
   }
 ]
