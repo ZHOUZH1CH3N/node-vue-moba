9defdb3c9433a4d670a7d3b5f187f612e301d1e6
1393925530 <zzc1393925530@gmail.com>
2020-01-16 20:53:23 +0800

    后台登陆处理

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,37 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 const http = axios.create({
   baseURL: 'http://localhost:3000/admin/api'
+})
+
+http.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  config.headers.Authorization = 'Bearer ' + sessionStorage.getItem('token')
+  return config;
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+});
+
+// Add a response interceptor
+axios.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response;
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error);
+});
+
+http.interceptors.response.use(res => {
+  return res
+}, err => {
+  if (err.response.data.message) {
+    Vue.prototype.$message.error(err.response.data.message)
+    return Promise.reject(err)
+  }
 })
 
 export default http
